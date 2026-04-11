273bbc4c1eccaf03b07731dc4acf71e824b5e1e3
Andres Abdala Alvarez <andrespampa@gmail.com>
2026-04-11 19:49:25 -0300

    Terminando con Styled-components Empezando Video 23

--- src/index.js
+++ src/index.js
@@ -2,30 +2,30 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import Usuario from './componentes/Usuario';
 import FormularioInicioSesion from './componentes/FormularioInicioSesion';
-import ContadorClass from './componentes/ContadorClass';
+// import ContadorClass from './componentes/ContadorClass';
 // import {useState} from 'react';
 import ContadorFuncional from './componentes/ContadorFuncional';
-
+import './index.css';
+import Boton from './elementos/Boton';
 
 
 const App = () =>{
 
-  const [sesion, cambiarEstadoSesion] = useState(false);
+  const [sesion, cambiarEstadoSesion] = useState(true);
 
   return(
-    <div>
+    <div className='contenedor'>
     {/* NO es la forma adecuada trabajar con estilos en linea */}
     {/*Segunda forma de hacerlo y mejor forma de hacerlo*/}
     {sesion === true ? 
     <div>
       <Usuario /> 
-      <ContadorClass cantidadAIncrementar={10} cantidadADisminuir={2} />
-      {/* <ContadorFuncional cantidadAIncrementar={10} cantidadADisminuir={2} /> */}
-      <button onClick = {() => cambiarEstadoSesion(false)}>Cerrar sesión</button>
+      {/* <ContadorClass cantidadAIncrementar={10} cantidadADisminuir={2} /> */}
+      <ContadorFuncional cantidadAIncrementar={10} cantidadADisminuir={2} />
+      <Boton largo marginTop marginRight className='boton' onClick = {() => cambiarEstadoSesion(false)}>Cerrar sesión</Boton>
     </div>
     :
     <div>
-      <p>No has iniciado sesión</p>
       <FormularioInicioSesion cambiarEstadoSesion={cambiarEstadoSesion} />
       {/* <button onClick={() => cambiarEstadoSesion(true)}>Iniciar sesión</button> */}
     </div>}
